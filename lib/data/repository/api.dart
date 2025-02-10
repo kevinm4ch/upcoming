7aef78b062358b63033533087821ccdb74721060
Kevin Machado <kevinm4ch@gmail.com>
2025-02-10 18:46:59 -0300

    mais um problema com gitignore

--- lib/data/repository/api.dart
+++
@@ -1,5 +0,0 @@
-class Api {
-  static const baseUrl = "https://api.themoviedb.org/3";
-  static const token =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWFmZDI4MzIwNjAyYzQwMzQxZTAxOTExYmExMWE1ZSIsIm5iZiI6MTcyNjE0ODE3MS4xMTM5OTk4LCJzdWIiOiI2NmUyZWU0YjI4MDQ4ZDkyZGVmOGZmMGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VqYorvdJ8ghTiTeHiRyB_XwCYFNiWnVZWeNh-jDrpyI";
-}
